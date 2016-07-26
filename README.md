b87dccdf39b23a791542d2f701a1a7b0edd0889d
dqhieu13 <dqhieu13@gmail.com>
2016-07-26 17:37:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# keyboard symbol
+
+**keyboard- ymbol** is a plugin for [BitBar](https://github.com/matryer/bitbar)
---
+++ keyboard-symbol.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# <bitbar.title>Keyboard symbol</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Đinh Quang Hiếu</bitbar.author>
+# <bitbar.author.github>dqhieu</bitbar.author.github>
+# <bitbar.desc>This plugin displays a menu of keyboard symbol</bitbar.desc>
+# <bitbar.image></bitbar.image>
+# <bitbar.dependencies></bitbar.dependencies>
+
+import sys, os
+
+print '⌨️'
+print '---'
+print '⌘ Command'
+print '⇧ Shift'
+print '⌫ Backspace/Delete'
+print '⇥ Tab'
+print '⇪ Caps lock'
+print '⌥ Option/Alt'
+print '⌃ Control'
+print '⎋ Escape'
+print '←↑→↓ Arrow keys'
+print '↩ Return'
